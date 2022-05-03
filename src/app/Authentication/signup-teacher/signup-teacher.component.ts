8fd79d602f2154f163a76de4b40218860dd3eef1
Oscar Avila <oscaravilaa9817@gmail.com>
2022-05-02 19:14:28 -0500

    creation of signup teacher

--- src/app/Authentication/signup-teacher/signup-teacher.component.ts
+++ src/app/Authentication/signup-teacher/signup-teacher.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { ACrudService } from '../shared/acrud.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-teacher',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupTeacherComponent implements OnInit {
 
-  constructor() { }
+  isLoading = false;
+  error: string = null;
+
+  constructor(
+    private authService: AuthService,
+    private acrud: ACrudService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSignup(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+    this.authService.SignUpTeacher(email, password).then(d => {
+          this.isLoading = false
+          this.authService.logout()
+        })
+          .catch(e => {
+            this.authService.logout()
+            this.isLoading = false
+            this.error = e
+          })
   }
 
 }
